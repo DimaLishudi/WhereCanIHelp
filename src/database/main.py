e76b223b98ada2d2c9c8135b6a4e9589c1b9a635
gvrsrg <gvrsrg@gmail.com>
2024-06-30 13:52:23 +0300

    database methods added

--- src/database/main.py
+++ src/database/main.py
@@ -1,39 +1,76 @@
 from queries.orm import SyncORM
 
-SyncORM.create_tables()
-
-org1 = SyncORM.insert_org(name="Helping Hands", phone="555-1234")
-org2 = SyncORM.insert_org(name="Community Care", phone="555-5678")
-org3 = SyncORM.insert_org(name="Bright Future", phone="555-9101")
-org4 = SyncORM.insert_org(name="Hope Foundation", phone="555-1122")
-org5 = SyncORM.insert_org(name="Unity Volunteers", phone="555-3344")
-org6 = SyncORM.insert_org(name="Peaceful World", phone="555-5566")
-org7 = SyncORM.insert_org(name="Green Earth", phone="555-7788")
-org8 = SyncORM.insert_org(name="Happy Hearts", phone="555-9900")
-org9 = SyncORM.insert_org(name="Lending Hands", phone="555-2233")
-org10 = SyncORM.insert_org(name="Sunny Days", phone="555-4455")
-
-need1 = SyncORM.insert_needs(name="Money")
-need2 = SyncORM.insert_needs(name="Food")
-need3 = SyncORM.insert_needs(name="Clothes")
-need4 = SyncORM.insert_needs(name="Volunteers")
-need5 = SyncORM.insert_needs(name="Medical Supplies")
-need6 = SyncORM.insert_needs(name="Educational Materials")
-need7 = SyncORM.insert_needs(name="Transportation")
-need8 = SyncORM.insert_needs(name="Office Supplies")
-need9 = SyncORM.insert_needs(name="Cleaning Supplies")
-need10 = SyncORM.insert_needs(name="Event Space")
-
-# SyncORM.insert_org_need(org1, need1)
-# SyncORM.insert_org_need(org1, need2)
-# SyncORM.insert_org_need(org1, need3)
-# SyncORM.insert_org_need(org1, need4)
-# SyncORM.insert_org_need(org2, need5)
-# SyncORM.insert_org_need(org2, need6)
-# SyncORM.insert_org_need(org2, need7)
-# SyncORM.insert_org_need(org2, need8)
 
 
-org = SyncORM.get_org_by_id(5)
-print(org)
 
+def main():
+    # SyncORM.create_tables()
+
+    # org1 = SyncORM.insert_org(name="Helping Hands", phone="555-1234")
+    # org2 = SyncORM.insert_org(name="Community Care", phone="555-5678")
+    # org3 = SyncORM.insert_org(name="Bright Future", phone="555-9101")
+    # org4 = SyncORM.insert_org(name="Hope Foundation", phone="555-1122")
+    # org5 = SyncORM.insert_org(name="Unity Volunteers", phone="555-3344")
+    # org6 = SyncORM.insert_org(name="Peaceful World", phone="555-5566")
+    # org7 = SyncORM.insert_org(name="Green Earth", phone="555-7788")
+    # org8 = SyncORM.insert_org(name="Happy Hearts", phone="555-9900")
+    # org9 = SyncORM.insert_org(name="Lending Hands", phone="555-2233")
+    # org10 = SyncORM.insert_org(name="Sunny Days", phone="555-4455")
+
+    # need1 = SyncORM.insert_needs(name="Money")
+    # need2 = SyncORM.insert_needs(name="Food")
+    # need3 = SyncORM.insert_needs(name="Clothes")
+    # need4 = SyncORM.insert_needs(name="Volunteers")
+    # need5 = SyncORM.insert_needs(name="Medical Supplies")
+    # need6 = SyncORM.insert_needs(name="Educational Materials")
+    # need7 = SyncORM.insert_needs(name="Transportation")
+    # need8 = SyncORM.insert_needs(name="Office Supplies")
+    # need9 = SyncORM.insert_needs(name="Cleaning Supplies")
+    # need10 = SyncORM.insert_needs(name="Event Space")
+
+    # SyncORM.insert_org_need(org1, need1)
+    # SyncORM.insert_org_need(org1, need2)
+    # SyncORM.insert_org_need(org1, need3)
+    # SyncORM.insert_org_need(org1, need4)
+    # SyncORM.insert_org_need(org2, need5)
+    # SyncORM.insert_org_need(org2, need6)
+    # SyncORM.insert_org_need(org2, need7)
+    # SyncORM.insert_org_need(org2, need8)
+
+    # adam = SyncORM.insert_user("Adam","123456")
+    # benjamin = SyncORM.insert_user("Benjamin", "5550987")
+
+    # SyncORM.add_user_capability(adam, need1)
+    # SyncORM.add_user_capability(adam, need2)
+    # SyncORM.add_user_capability(adam, need3)
+    # SyncORM.add_user_capability(adam, need4)
+    # SyncORM.add_user_capability(benjamin, need5)
+    # SyncORM.add_user_capability(benjamin, need6)
+    # SyncORM.add_user_capability(benjamin, need7)
+    # SyncORM.add_user_capability(benjamin, need8)
+    # SyncORM.add_user_capability(benjamin, need9)
+    # SyncORM.add_user_capability(benjamin, need10, "Owns Concert Hall")
+
+    # SyncORM.insert_action(adam, org1, need1)
+    # SyncORM.insert_action(adam, org1, need2)
+    # SyncORM.insert_action(adam, org2, need3)
+    # SyncORM.insert_action(adam, org2, need4, amount=4)
+    # SyncORM.insert_action(benjamin, org2, need5)
+    # SyncORM.insert_action(benjamin, org2, need6)
+    # SyncORM.insert_action(benjamin, org1, need7)
+    # SyncORM.insert_action(benjamin, org1, need8)
+    # SyncORM.insert_action(benjamin, org1, need9)
+    # SyncORM.insert_action(benjamin, org1, need10, descsription="Concert Hall every other Tuesday 13:00 to 18:00")
+    org = SyncORM.get_org_by_id(5)
+    print(org)
+
+    benjamin = SyncORM.get_user_by_id(2)
+
+
+    ben_actions = SyncORM.get_user_actions(benjamin)
+    for action in ben_actions:
+        print(action)
+
+if __name__ == "__main__":
+    main()
+    
--- src/database/queries/orm.py
+++ src/database/queries/orm.py
@@ -1,8 +1,9 @@
+from datetime import date
 from sqlalchemy import Integer, and_, cast, func, insert, inspect, or_, select, text
 from sqlalchemy.orm import aliased, contains_eager, joinedload, selectinload
 
 from database import Base, session_factory, engine
-from models_orm import Organization, Helptype, OrgNeeds, User, Actions
+from models_orm import Organization, Helptype, OrgNeeds, User, UserСapability, Actions
 
 class SyncORM:
     @staticmethod
@@ -69,10 +70,66 @@ class SyncORM:
             session.add(new_need)
             session.commit()
 
+    @staticmethod
+    def insert_user(name, phone, email=""):
+        with session_factory() as session:
+            session.expire_on_commit = False
+            new_user = User()
+            new_user.name = name
+            new_user.phone = phone
+            new_user.email = email
+            session.add(new_user)
+            session.flush()
+            session.commit()
+            session.expunge(new_user)
+            return new_user
+        
+    @staticmethod
+    def add_user_capability(user:User, helptype:Helptype, amount=1, description=""):
+        with session_factory() as session:
+            session.expire_on_commit = False
+            new_cap = UserСapability()
+            new_cap.user_id = user.id
+            new_cap.help_type_id = helptype.id
+            new_cap.description = description
+            new_cap.amount = amount
+            session.add(new_cap)
+            session.commit()
 
-def update_organization(organization_id: int = 2, new_username: str = "Misha"):
-    with session_factory() as session:
-        worker_michael = session.get(Organization, organization_id)
-        worker_michael.name = new_username
-        session.refresh(worker_michael)
-        session.commit()
+
+
+    @staticmethod
+    def insert_action(user:User, org:Organization, need:Helptype, amount=1, action_date=date.today(), descsription=""):
+        with session_factory() as session:
+            session.expire_on_commit = False
+            new_action = Actions()
+            new_action.organization_id = org.id
+            new_action.user_id = user.id
+            new_action.help_type_id = need.id
+            new_action.amount = amount
+            new_action.action_date = action_date
+            new_action.description = descsription
+            session.add(new_action)
+            session.flush()
+            session.commit()
+            session.expunge(new_action)
+            return new_action
+    
+    @staticmethod
+    def get_user_actions(user):
+        with session_factory() as session:
+            query = select(Actions).where(Actions.user_id == user.id)
+            result = session.execute(query)
+            actions = result.scalars().all()
+            return actions
+        
+    @staticmethod
+    def get_user_by_id(id):
+        with session_factory() as session:
+            session.expire_on_commit = False
+            user = session.get(User, id)
+            session.expunge_all()
+            return user
+
+    
+
